c5b8f7178f43224ac011beb5478cdaa59ee99055
3000Studios <mr.jwswain@gmail.com>
2026-04-23 17:31:19 -0400

    Move synth wallpaper to admin

--- frontend/components/admin/AdminChrome.jsx
+++ frontend/components/admin/AdminChrome.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AuroraBackdrop from '../../backgrounds/AuroraBackdrop.jsx'
 import StarClusterSynth from '../../backgrounds/StarClusterSynth.jsx'
+import SynthWaveWallpaper from '../../backgrounds/SynthWaveWallpaper.jsx'
 
 export default function AdminChrome() {
   return (
     <>
       <AuroraBackdrop variant="public" />
+      <SynthWaveWallpaper opacity={0.5} />
       <StarClusterSynth density={0.7} />
     </>
   )
